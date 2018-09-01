57da6087f1ae074e05e542a203c9b410e918a1b8
397442219 <maozipeng365365>
2018-09-01 12:59:55 +0800

    1
    1

---
+++ server.js
@@ -0,0 +1,20 @@
+const Koa =require('koa');
+const app = new Koa();
+app.use(async(ctx,next)=>{
+	console.log(1)
+	next();
+	console.log(2)
+})
+app.use(async(ctx,next)=>{
+	console.log(3)
+	next();
+	console.log(4)
+})
+app.use(async(ctx,next)=>{
+	console.log(5)
+	next();
+	console.log(6)
+})
+
+
+app.listen(3000)
